0ce4fdd6044bf7395ea59ec9491e660064954325
u_danillu <danillo.cruz@fatec.sp.gov.br>
2023-04-06 18:56:25 -0300

    Primeiro Commit

---
+++ models/post.js
@@ -0,0 +1,24 @@
+const { type } = require("os");
+const db = require("./banco");
+
+const Agendamentos = db.sequelize.define("agendamentos", {
+    nome: {
+      type: db.Sequelize.STRING,
+    },
+    telefone: {
+      type: db.Sequelize.INTEGER,
+    },
+    origem: {
+      type: db.Sequelize.STRING,
+    },
+    data_contato: {
+      type: db.Sequelize.DATE,
+    },
+    observacao: {
+      type: db.Sequelize.TEXT,
+    },
+});
+
+//Agendamentos.sync({force: true})
+
+module.exports = Agendamentos;
